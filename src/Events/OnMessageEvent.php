74a24a3ebedef54c6bf5f4de1cccdac7d1a5ab3d
6xiaowu9 <736518585@qq.com>
2020-06-16 11:08:46 +0800

    update composer config

--- src/Events/OnMessageEvent.php
+++ src/Events/OnMessageEvent.php
@@ -7,10 +7,13 @@ use Swoole\Websocket\Frame;
 use NeoP\Server\Annotation\Mapping\SwooleEvent;
 use NeoP\WebSocket\Server\Events\EventType;
 
+/**
+ * @SwooleEvent(EventType::ON_MESSAGE, type=EventType::LISTEN_WEBSOCKET)
+ */
 class OnMessageEvent
 {
     public function handler(Server $server, Frame $frame)
     {
-        var_dump("__ON_MESSAGE__, workerId: " . $server->worker_id);
+        // var_dump("__ON_MESSAGE__, workerId: " . $server->worker_id);
     }
 }
